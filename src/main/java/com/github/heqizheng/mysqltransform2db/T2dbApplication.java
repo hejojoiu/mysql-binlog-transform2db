e3bd406ccb3739306a95d67764383e24d988688f
he.junjie@lianduoduo.com <hjj123456>
2022-03-28 16:50:25 +0800

    doc:增加启动类用于测试

---
+++ src/main/java/com/github/heqizheng/mysqltransform2db/T2dbApplication.java
@@ -0,0 +1,17 @@
+package com.github.heqizheng.mysqltransform2db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author heqizheng
+ * @version 1.0
+ * @date 2022/3/28
+ */
+@SpringBootApplication
+public class T2dbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T2dbApplication.class, args);
+    }
+}
